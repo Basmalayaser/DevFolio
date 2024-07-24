e525e3bb736e72f573fe4346ae71fe0034ab7972
BasmalaYasser <103972624+Basmalayaser@users.noreply.github.com>
2024-07-24 21:28:23 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+  var typed = new Typed('#typed', {
+    strings: ['Designer', 'Developer','Freelancer','Photographer'],
+    typeSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    smartBackspace: false,
+    backSpeed: 50,
+  });
